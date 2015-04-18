67478ab088e20cf880c637e8a3a1f9cc62a4aedb
Ryan Neufeld <ryan@rkn.io>
2015-04-18 18:53:32 -0500

    Release 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject sim/lein-template "0.1.0-SNAPSHOT"
+(defproject sim/lein-template "0.1.0"
   :description "Simulant project generator"
   :url "https://github.com/homegrownlabs/sim-template"
   :license {:name "The MIT License"
